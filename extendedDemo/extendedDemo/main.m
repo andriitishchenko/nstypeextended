2d652102a882e89aa81f3978b0b274d18fbe2e5f
Andrii Tishchenko <andrii.tishchenko+github@gmail.com>
2014-07-22 20:59:51 +0300

    added demo

---
+++ extendedDemo/extendedDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  extendedDemo
+//
+//  Created by Andrii Tishchenko on 03.02.14.
+//  Copyright (c) 2014 Andrii Tishchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ extendedDemo/extendedDemoTests/extendedDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  extendedDemoTests.m
+//  extendedDemoTests
+//
+//  Created by Andrii Tishchenko on 03.02.14.
+//  Copyright (c) 2014 Andrii Tishchenko. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface extendedDemoTests : XCTestCase
+
+@end
+
+@implementation extendedDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
