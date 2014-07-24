290df7ebebe61927ac9a1c667fb4a63613f3cacd
Andrii Tishchenko <andrii.tishchenko+github@gmail.com>
2014-07-24 22:31:56 +0300

    UITextField category updated

--- src/UITextField+UITextField_padding.m
+++ src/UITextField+UITextField_padding.m
@@ -12,11 +12,24 @@
 @implementation UITextField (UITextField_padding)
 // placeholder position
 - (CGRect)textRectForBounds:(CGRect)bounds {
-    return CGRectInset( bounds , UITextFieldPadding , UITextFieldPadding );
+    return [self rectForBounds:bounds];
+    //CGRectInset( bounds , UITextFieldPadding , UITextFieldPadding );
 }
 
 // text position
 - (CGRect)editingRectForBounds:(CGRect)bounds {
-    return CGRectInset( bounds , UITextFieldPadding , UITextFieldPadding );
+    return [self rectForBounds:bounds];
+}
+
+-(CGRect)rectForBounds:(CGRect)bounds
+{
+    CGRect b1 = CGRectInset( bounds , UITextFieldPadding , UITextFieldPadding );
+    if (self.leftView) {
+        CGRect b2 =self.leftView.bounds;
+        b1.origin.x = b1.origin.x+b2.size.width;
+        b1.size.width = b1.size.width-b2.size.width;
+        
+    }
+    return b1;
 }
 @end
